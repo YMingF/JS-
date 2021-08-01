db20bbef170a020d6a25b1688888846b0c57c2e0
YM <a834319313@163.con>
2021-08-01 19:12:25 +0800

    前航上-使用hashMap和localStorage

--- dist/index.50068c4e.js
+++ dist/index.50068c4e.js
@@ -442,6 +442,31 @@ id) /*: string*/
 }
 
 },{}],"4lDoZ":[function(require,module,exports) {
+const $siteList=$('.siteList')
+const $lastLi=$siteList.find('li.last')//到li里找类为last的元素
+const x=localStorage.getItem('x')
+const xObject=JSON.parse(x)
+//用数组
+const hashMap=xObject || [  //一开始xObject可能为空,所以要用||，即如果xObject为空,那就去用后面的数据赋值
+    {logo:'A' ,logoType:'text',url:'https://www.acfun.cn' },
+    {logo: './images/bilibili.png',logoType:'img',url:'https://www.bilibili.com/' },
+]
+const render=()=>{
+    $siteList.find('li:not(.last)').remove()//将之前界面显示的网址都删除,方便后面重新显示,有点像Python的界面刷新的功能
+    hashMap.forEach(node=>{
+        const $li=$(`
+        <li>
+        <a href="${node.url}">
+        <div class="site">
+            <div class="logo">${node.logo[0]}</div>
+            <div class="link">${node.url}</div>
+        </div>
+        </a>
+    </li>
+        `).insertBefore($lastLi)//将新元素插到lastLi前面
+    })
+}
+render()
 //监听点击事件
 $('.addButton')
     .on('click',()=>{
@@ -450,19 +475,14 @@ $('.addButton')
            url='https://'+url
         }
         console.log(url)
-        const $siteList=$('.siteList')
-        const $lastLi=$siteList.find('li.last')//到li里找类为last的元素
-        const $li=$(`
-        <li>
-          <a href="${url}">
-            <div class="site">
-              <div class="logo">${url[0]}</div>
-              <div class="link">${url}</div>
-            </div>
-          </a>
-        </li>
-        `).insertBefore($lastLi)//将新元素插到lastLi前面
-    })
+        hashMap.push({logo:url[0],logoType:'text',url:url})
+        render()
+});
+
+window.onbeforeunload=()=>{//在你关闭或刷新页面时触发
+    const string=JSON.stringify(hashMap)//将对象转换成字符串
+    localStorage.setItem('x',string)//在本地的存储里设置一个x ，值为string
+}
 },{}]},["1PNcb","4lDoZ"], "4lDoZ", "parcelRequire427e")
 
 //# sourceMappingURL=index.50068c4e.js.map
